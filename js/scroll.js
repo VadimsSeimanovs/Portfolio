47f67b88bcf5fb73c16073b3979867777afc3da3
Vadims Seimanovs <vadims.seimanovs@gmail.com>
2020-07-30 17:23:50 +0100

    style(index): add new info block

--- js/scroll.js
+++ js/scroll.js
@@ -1,66 +1,88 @@
 var currentScreen = "first-screen";
+
+function pageUpCheck(){
+    switch (currentScreen) {
+        case "first-screen":
+            setTimeout(function(){
+                document.getElementById("first-screen").scrollIntoView({behavior: "smooth"});
+                currentScreen = "first-screen";
+                //document.getElementById("up-screen").style.display = "none"; This needs to be reworked
+            }, 500);
+            break;
+        case "second-screen":
+            setTimeout(function () {
+                document.getElementById("first-screen").scrollIntoView({behavior: "smooth"});
+                currentScreen = "first-screen";
+            }, 500)
+            break;
+        case "third-screen":
+            setTimeout(function () {
+                document.getElementById("second-screen").scrollIntoView({behavior: "smooth"});
+                currentScreen = "second-screen";
+            }, 500)
+            break;
+        case "fourth-screen":
+            setTimeout(function () {
+                document.getElementById("third-screen").scrollIntoView({behavior: "smooth"});
+                currentScreen = "third-screen";
+            },500)
+            break;
+        case "fifth-screen":
+            setTimeout(function () {
+                document.getElementById("fourth-screen").scrollIntoView({behavior: "smooth"});
+                currentScreen = "fourth-screen";
+            }, 500)
+            break;
+        case "sixth-screen":
+            setTimeout(function () {
+                document.getElementById("fifth-screen").scrollIntoView({behavior: "smooth"});
+                currentScreen = "fifth-screen";
+            }, 500)
+            break;
+    }
+}
+
+function pageDownCheck(){
+    switch (currentScreen) {
+        case "first-screen":
+            setTimeout(function () {
+                document.getElementById("second-screen").scrollIntoView({behavior: "smooth"});
+                currentScreen = "second-screen";
+            }, 500);
+            break;
+        case "second-screen":
+            setTimeout(function () {
+                document.getElementById("third-screen").scrollIntoView({behavior: "smooth"});
+                currentScreen = "third-screen";
+            }, 500)
+            break;
+        case "third-screen":
+            setTimeout(function () {
+                document.getElementById("fourth-screen").scrollIntoView({behavior: "smooth"});
+                currentScreen = "fourth-screen";
+            }, 500)
+            break;
+        case "fourth-screen":
+            setTimeout(function () {
+                document.getElementById("fifth-screen").scrollIntoView({behavior: "smooth"});
+                currentScreen = "fifth-screen";
+            }, 500)
+            break;
+        case "fifth-screen":
+            setTimeout(function () {
+                document.getElementById("sixth-screen").scrollIntoView({behavior: "smooth"});
+                currentScreen = "sixth-screen";
+            }, 500)
+            break;
+    }
+}
+
 function mouseScroll(event){
     if (event.deltaY <= -25){
-        switch (currentScreen) {
-            case "first-screen":
-                setTimeout(function(){
-                    document.getElementById("first-screen").scrollIntoView(true);
-                    currentScreen = "first-screen";
-                }, 500);
-                break;
-            case "second-screen":
-                setTimeout(function () {
-                    document.getElementById("first-screen").scrollIntoView(true);
-                    currentScreen = "first-screen";
-                }, 500)
-                break;
-            case "third-screen":
-                setTimeout(function () {
-                    document.getElementById("second-screen").scrollIntoView(true);
-                    currentScreen = "second-screen";
-                }, 500)
-                break;
-            case "sixth-screen":
-                setTimeout(function () {
-                    document.getElementById("third-screen").scrollIntoView(true);
-                    currentScreen = "third-screen";
-                },500)
-                break;
-            case "fourth-screen":
-                setTimeout(function () {
-                    document.getElementById("sixth-screen").scrollIntoView(true);
-                    currentScreen = "sixth-screen";
-                }, 500)
-                break;
-        }
+        pageUpCheck();
     }
     if (event.deltaY >= 25){
-        switch (currentScreen) {
-            case "first-screen":
-                setTimeout(function () {
-                    document.getElementById("second-screen").scrollIntoView(true);
-                    currentScreen = "second-screen";
-                }, 500);
-                break;
-            case "second-screen":
-                setTimeout(function () {
-                    document.getElementById("third-screen").scrollIntoView(true);
-                    currentScreen = "third-screen";
-                }, 500)
-                break;
-            case "third-screen":
-                setTimeout(function () {
-                    document.getElementById("sixth-screen").scrollIntoView(true);
-                    currentScreen = "sixth-screen";
-                }, 500)
-                break;
-            case "sixth-screen":
-                setTimeout(function () {
-                    document.getElementById("fourth-screen").scrollIntoView(true);
-                    currentScreen = "fourth-screen";
-                }, 500)
-                break;
-        }
+        pageDownCheck();
     }
-    console.log(currentScreen);
 }
+

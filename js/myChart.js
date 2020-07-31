406955c244ff21f1db94185ab75d4eedadf8a4a7
Vadims Seimanovs <vadims.seimanovs@gmail.com>
2020-07-31 15:44:49 +0100

    style(char): pie chart

--- js/myChart.js
+++ js/myChart.js
@@ -8,11 +8,11 @@ new Chart(document.getElementById("my-skills"), {
         }
     },
     "data": {
-        "labels": ["JavaScript", "PHP", "Java", "C", "C++", "Others"],
+        "labels": ["C#","JavaScript", "PHP", "Java", "C", "C++", "Others"],
         "datasets": [{
             "label": "Programming Languages",
-            "data": [30, 20, 30, 7, 7, 6],
-            "backgroundColor": ["rgb(255,184,0)", "rgb(233,0,255)", "rgb(20,151,0)", "rgb(0,25,151)", "rgb(151,30,0)", "rgb(255,128,0)"]
+            "data": [25,20, 15, 20, 7, 7, 6],
+            "backgroundColor": ["rgb(255,3,3)" ,"rgb(255,184,0)", "rgb(233,0,255)", "rgb(20,151,0)", "rgb(0,25,151)", "rgb(151,30,0)", "rgb(255,128,0)"]
         }]
     }
 });
